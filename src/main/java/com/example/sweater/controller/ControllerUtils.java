f7e56e1caac45de4727cce5fd46cc56629055236
Albert <fruitlesskey2@mail.ru>
2023-01-22 04:09:19 +0300

    first commit

---
+++ src/main/java/com/example/sweater/controller/ControllerUtils.java
@@ -0,0 +1,43 @@
+package com.example.sweater.controller;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.*;
+
+public class ControllerUtils {
+
+    static Map<String, List<String>> getErrors(BindingResult bindingResult) {
+
+
+
+        //хранит по каждому ключу(полю) несколько сообщений об ошибке
+        Map<String, List<String>> allErrors = new HashMap<>();
+
+
+
+
+        for (FieldError error : bindingResult.getFieldErrors()) {
+
+
+
+            //ключ для Map
+            String key = error.getField() + "Error";
+
+
+
+
+            if (allErrors.containsKey(key)) {
+
+
+                //добавляем по этому ключу новое сообщение с ошибкой
+                allErrors.get(key).add(error.getDefaultMessage());
+            } else {
+
+                allErrors.put(key, new ArrayList<>(Arrays.asList(error.getDefaultMessage())));
+            }
+        }
+
+        return allErrors;
+    }
+}
---
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -0,0 +1,86 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.User;
+import com.example.sweater.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(
+            @RequestParam("password2") String passwordConfirm,
+            @Valid User user,
+            BindingResult bindingResult,
+            Model model) {
+
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+
+        if (isConfirmEmpty) {
+            model.addAttribute("password2Error", "Password confirmation cannot be empty");
+        }
+
+        if (user.getPassword() != null && !user.getPassword().equals(passwordConfirm)) {
+            model.addAttribute("passwordError", "Passwords are different!");
+            return "registration";
+        }
+        if (isConfirmEmpty || bindingResult.hasErrors()) {
+            Map<String, List<String>> errors = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errors);
+
+            return "registration";
+        }
+        boolean containAnyError = model.asMap().keySet().stream()
+                .anyMatch(key -> key.contains("Error"));
+        if (containAnyError) {
+            return "registration";
+        }
+        if (!userService.AddUser(user)) {
+            model.addAttribute("usernameError", "User exists!");
+            return "registration";
+        }
+
+        return "redirect:/login";
+    }
+//        if (!userService.AddUser(user)) {
+//            model.addAttribute("usernameError", "User exists!");
+//            return "registration";
+//        }
+//
+//        return "redirect:/login";
+//    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code) {
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("messageType", "success");
+            model.addAttribute("message", "User successfully activated");
+        } else {
+            model.addAttribute("messageType", "danger");
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
+    }
+}
